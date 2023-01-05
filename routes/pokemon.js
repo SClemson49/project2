d39d4ccc5defa855e3fc0ee8fb4c662711f6969f
SClemson49 <s.clemson49@gmail.com>
2023-01-05 11:29:35 -0900

    updating...

---
+++ routes/pokemon.js
@@ -0,0 +1,16 @@
+const { default: axios } =require('axios')
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+
+
+router.get('/', async (req, res) =>{
+    try {
+        // to do : get pokemon in parties and render to views
+    }
+})
+
+// router.post
+
+
+module.exports = router
